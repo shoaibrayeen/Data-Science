f9f6617da280d542a26d0bc21e9f6a2c46f445ea
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-07-31 01:58:54 +0530

    Basic Arithmetic Operations and printing Variable Type

---
+++ Python/1. Introduction to Python/script.py
@@ -0,0 +1,61 @@
+# An addition
+
+print(6 + 12)
+
+# A subtraction
+print(6 - 12)
+
+# A multiplication
+print(3 * 5)
+
+ # A division
+print((5 + 5) / 2)
+
+
+# Exponentiation
+print(2 ^ 5)
+
+
+# Modulo
+print(28 % 6)
+
+# Assign the value 42 to x
+x = 42
+
+# Print out the value of the variable x
+print(x)
+
+
+
+# Assign the value 5 to the variable my_apples
+
+my_apples = 5
+# Print out the value of the variable my_apples
+print(my_apples)
+
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples = 5
+
+my_oranges = 6
+# Add these two variables together
+
+print(my_oranges + my_apples)
+# Create the variable my_fruit
+my_fruit = my_oranges + my_apples
+
+
+
+# Declare variables of different types
+my_numeric = 42
+my_character = "universe"
+my_logical = FALSE 
+
+# Check class of my_numeric
+type(my_numeric)
+
+# Check class of my_character
+type(my_character)
+
+# Check class of my_logical
+type(my_logical)
