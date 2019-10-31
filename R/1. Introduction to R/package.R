3371b37b6e909a3b5659a9b7c87bce4f06e3378b
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-01 00:54:22 +0530

    Installing, Uninstalling, Using and Listing the packages

---
+++ R/1. Introduction to R/package.R
@@ -0,0 +1,10 @@
+
+#installing package
+
+install.packages("Name")
+#using the package
+liberary("Name")
+#uninstalling package
+uninstall.packages("Name")
+#List all the installed packages
+library()
