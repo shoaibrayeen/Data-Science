bf4c7e1600bb5d89ad8f1edd77bafc7603290be4
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-01 00:57:50 +0530

    Data Type of Different Variables

---
+++ R/1. Introduction to R/dataType.R
@@ -0,0 +1,27 @@
+# Data Types
+logicalVar <- TRUE 
+print(class(logicalVar))
+
+numericVar1 <- 23.5
+print(class(numericVar1))
+
+numericVar2 <- 23
+print(class(numericVar2))
+
+
+integerVar <- 2L
+print(class(integerVar))
+
+
+complexVar <- 2+5i
+print(class(complexVar))
+
+stringVar <- "Hello World"
+print(class(stringVar))
+
+# Store as ASCII Value of A Character
+rawVar <- charToRaw("Hello")
+print(class(rawVar))
+print(rawVar)
+
+
