7a72d0d9cb17043cb10624afeba7f8763c96eccd
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-11-01 00:56:36 +0530

    Operations on String

---
+++ R/1. Introduction to R/string.R
@@ -0,0 +1,14 @@
+# Case Change
+convertVar4 <- toupper("Changing To Upper")
+print(convertVar4)
+
+convertVar5 <- tolower("CHANGING TO Lower")
+print(convertVar5)
+
+# Substring
+subVar <- substring("Hello World", 5, 9)  # 5 to 9
+print(subVar)
+
+# Count Char
+countVar <- nchar("Count the number of characters")
+print(countVar)
